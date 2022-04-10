074afceb24f05a0d6bc6bb39d0876fe182761da4
r-kurat <lophax09@gmail.com>
2022-04-10 21:10:59 +0900

    wip

--- .github/workflows/lib/main.ts
+++ .github/workflows/lib/main.ts
@@ -1,6 +1,23 @@
-export const main = () => {
-  console.log("hoge");
-  return 2;
+import { readFileSync } from "node:fs";
+import github from "@actions/github";
+import core from "@actions/core";
+
+export const main = async () => {
+  const myToken = core.getInput("myToken");
+
+  const octokit = github.getOctokit(myToken);
+  const context = github.context;
+
+  const coverageSummary = JSON.parse(
+    readFileSync("./coverage/coverage-summary.json", "utf8")
+  );
+
+  await octokit.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: "👋 ",
+  });
 };
 
 main();
