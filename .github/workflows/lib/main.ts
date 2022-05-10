ee83f7a03a2002821df8624ed7ce21f95c0fd031
r-kurat <ryosuke.kuratsu@legalforce.co.jp>
2022-05-10 21:10:12 +0900

    test: summaryのテスト

--- .github/workflows/lib/main.ts
+++ .github/workflows/lib/main.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from "node:fs";
 import { getOctokit, context } from "@actions/github";
+import { summary } from "@actions/core";
 
 export const main = async () => {
   const myToken = process.env.myToken;
@@ -13,6 +14,42 @@ export const main = async () => {
   const coverageSummary = JSON.parse(
     readFileSync("./coverage/coverage-summary.json", "utf8")
   );
+
+  await summary
+    .addHeading("Test Results")
+    .addTable([
+      [
+        { data: "Status", header: true },
+        { data: "Category", header: true },
+        { data: "Percentage", header: true },
+        { data: "Covered/Total", header: true },
+      ],
+      [
+        "🟢",
+        "lines",
+        coverageSummary.total.lines.pct,
+        `${coverageSummary.total.lines.covered} / ${coverageSummary.total.lines.total}`,
+      ],
+      [
+        "🟢",
+        "functions",
+        coverageSummary.total.functions.pct,
+        `${coverageSummary.total.functions.covered} / ${coverageSummary.total.functions.total}`,
+      ],
+      [
+        "🟢",
+        "statements",
+        coverageSummary.total.statements.pct,
+        `${coverageSummary.total.statements.covered} / ${coverageSummary.total.statements.total}`,
+      ],
+      [
+        "🟢",
+        "branches",
+        coverageSummary.total.branches.pct,
+        `${coverageSummary.total.branches.covered} / ${coverageSummary.total.branches.total}`,
+      ],
+    ])
+    .write();
 
   await octokit.rest.issues.createComment({
     issue_number: context.issue.number,
--- src/foo.ts
+++ src/foo.ts
@@ -1,4 +1,8 @@
 export const foo = (arg: number) => {
+  const _ = "a";
+
+  const _test = _ === "a" ? 1 : 2;
+
   if (arg === 0) return 0;
   if (arg === 1) return 1;
   return arg;
